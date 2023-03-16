d3717342138276612808792a97162b26cefa3b57
Hasan Marhoon <hasanmarhoon98@gmail.com>
2023-03-16 09:12:06 +0300

    created User, category, join table models and custom signup form

--- project3/main_app/admin.py
+++ project3/main_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Category, CustomUser, UserCategory
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(CustomUser)
+admin.site.register(UserCategory)
--- project3/main_app/views.py
+++ project3/main_app/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from main_app.forms import UserCreationForm
+from django.contrib.auth import login
+from django.shortcuts import redirect, render
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'home.html')
+
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        error_message = 'Invalid form entries'
+    form = UserCreationForm()
+    context = {'form': form, 'error': error_message}
+    return render(request, 'registration/signup.html', context)
+
+
+
+        
