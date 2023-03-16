8aa571db6b3e33ef042afb33abe7232a13818a21
Hasan Marhoon <hasanmarhoon98@gmail.com>
2023-03-16 13:15:31 +0300

    views and urls

--- project3/main_app/views.py
+++ project3/main_app/views.py
@@ -5,39 +5,49 @@ from django.shortcuts import redirect, render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Category, Quiz
+from .models import Category, Quiz, Exercise
 
 
-from django.contrib import messages #import messages
+from django.contrib import messages  # import messages
 
 
 # Create your views here.
 
+
 def index(request):
-    return render(request, 'home.html')
+    return render(request, "home.html")
+
 
 ## Sign up
 def signup(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('home')
+            return redirect("home")
         else:
-            messages.error(request,'Invalid form entries')
+            messages.error(request, "Invalid form entries")
     form = UserCreationForm()
-    context = {'form': form}
-    return render(request, 'registration/signup.html', context)
+    context = {"form": form}
+    return render(request, "registration/signup.html", context)
+
 
 class CategoryList(ListView):
     model = Category
+
 
 class CategoryDetail(DetailView):
     model = Category
 
 
-def take_quiz(request, category_id, quiz_id):
-    quiz = Quiz.objects.get(category_id=category_id)
+def take_quiz(request, category_id):
+    quiz = Quiz.objects.all().filter(category_id=category_id)
     # questions = [questions.append(q) for q in quiz ]
-    return render(request, 'main_app/quiz.html', {"quiz": quiz})
+    return render(request, "main_app/quiz.html", {"quiz": quiz})
+
+def take_exercise(request, category_id):
+    exercise = Exercise.objects.all().filter(category_id=category_id)
+    
+    # questions = [questions.append(q) for q in quiz ]
+    return render(request, "main_app/exercise.html", {"exercise": exercise})
