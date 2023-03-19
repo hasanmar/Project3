876bef70574db529ce5ab0ef1f36040a37385afb
Hasan Marhoon <hasanmarhoon98@gmail.com>
2023-03-19 14:00:17 +0300

    added score

--- project3/main_app/urls.py
+++ project3/main_app/urls.py
@@ -2,13 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('accounts/signup', views.signup, name='signup'),
-    path('accounts/login/', views.CustomLoginView.as_view(), name='login'),
-    path('categories', views.CategoryList.as_view(), name='categories'),
+    path("", views.index, name="home"),
+    path("accounts/signup", views.signup, name="signup"),
+    path("accounts/login/", views.CustomLoginView.as_view(), name="login"),
+    path("categories", views.CategoryList.as_view(), name="categories"),
     # path('category/<int:pk>', views.CategoryDetail.as_view(), name='categories_detail'),
-    path('category/<int:category_id>/quiz', views.take_quiz, name='quiz'),
-    path('category/<int:category_id>/exercise',
-         views.take_exercise,
-         name='exercise')
+    path("category/<int:category_id>/quiz", views.take_quiz, name="quiz"),
+    path("category/<int:category_id>/exercise", views.take_exercise, name="exercise"),
 ]
