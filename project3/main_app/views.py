12984c854f6d6438ca26cabf82e2d35a5d7932fb
Adulla Qassim <aboodyaq7@gmail.com>
2023-03-21 12:15:12 +0300

    fix conflict

--- project3/main_app/views.py
+++ project3/main_app/views.py
@@ -316,5 +316,3 @@ def activate(request, uidb64, token):
 class Profile(DetailView):
     model = CustomUser
     template_name = 'profile.html'
-    slug_field = 'username'
-    slug_url_kwarg = 'username'
