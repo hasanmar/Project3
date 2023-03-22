aade31257d787374435976622017a37c9979e5f6
Hasan Marhoon <hasanmarhoon98@gmail.com>
2023-03-22 09:32:11 +0300

    test

--- project3/main_app/views.py
+++ project3/main_app/views.py
@@ -14,7 +14,7 @@ from django.views.generic import ListView, DetailView, CreateView
 from django.utils.encoding import force_bytes, force_str  
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode  
 from .forms import UserCreationForm
-from .tokens import account_activation_token   
+from .tokens import account_activation_token
 
 
 
