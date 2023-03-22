5edda678ebb115aa4bad12afee80cbed1b836a2d
anas029 <aliyaqoob+github@gmail.com>
2023-03-22 09:28:54 +0300

    @anas029 profile page and css

--- project3/main_app/views.py
+++ project3/main_app/views.py
@@ -2,25 +2,28 @@ from django.contrib import messages
 from django.contrib.auth import login
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView
-from django.contrib.sites.shortcuts import get_current_site  
-from django.core.mail import EmailMessage  
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import EmailMessage
 from django.db.models.functions import Random
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
-from django.template.loader import render_to_string  
+from django.template.loader import render_to_string
 from django.urls import reverse_lazy
 from .models import Category, Quiz, Exercise, CustomUser, UserCategory
 from django.views.generic import ListView, DetailView, CreateView
-from django.utils.encoding import force_bytes, force_str  
-from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode  
+from django.views.generic.edit import UpdateView
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from .forms import UserCreationForm
-from .tokens import account_activation_token   
-
-
+from .tokens import account_activation_token
 
 
 def index(request):
     return render(request, "home.html")
+
+
+def learn(request):
+    return render(request, "learn.html")
 
 
 class CategoryList(ListView):
@@ -47,27 +50,29 @@ def take_quiz(request, category_id):
         all_answers = []
         score = 0
         counter = 0
-        for i in range(1,6):
+        for i in range(1, 6):
             answers.append(request.POST.get(f'answer{i}'))
         questions = request.session.get('quiz_id')
         for q in questions:
-            questionList.append(Quiz.objects.get(id = q)) 
-            correctAnswers.append(Quiz.objects.get(id = q).correctAnswer)
-        for a in answers: 
+            questionList.append(Quiz.objects.get(id=q))
+            correctAnswers.append(Quiz.objects.get(id=q).correctAnswer)
+        for a in answers:
             if a == correctAnswers[counter]:
                 score += 20
-                all_answers.append([True,questionList[counter].qustion])
+                all_answers.append([True, questionList[counter].qustion])
             elif a != correctAnswers[counter]:
-                all_answers.append([False,questionList[counter].qustion])
+                all_answers.append([False, questionList[counter].qustion])
             counter += 1
 
         user = request.user.id
         category = category_id
-        userLevel = CustomUser.objects.get(id = user)
-        usercat = UserCategory.objects.filter(user_id = user, category_id = category)
-        if usercat: 
-            usercat = UserCategory.objects.get(user_id = user, category_id = category)
-            usercat.attempts +=1
+        userLevel = CustomUser.objects.get(id=user)
+        usercat = UserCategory.objects.filter(user_id=user,
+                                              category_id=category)
+        if usercat:
+            usercat = UserCategory.objects.get(user_id=user,
+                                               category_id=category)
+            usercat.attempts += 1
             if score == 80:
                 userLevel.level += 0.2
                 usercat.level += 0.4
@@ -109,12 +114,10 @@ def take_quiz(request, category_id):
                 messages.warning(request, "Score too low, level unaffected.")
             userLevel.save()
             usercat.save()
-        return render(
-            request, 'main_app/score.html', {
-                'all_answers': all_answers,
-                'score': score
-            })
-
+        return render(request, 'main_app/score.html', {
+            'all_answers': all_answers,
+            'score': score
+        })
 
 
 ################################################
@@ -148,13 +151,13 @@ def take_exercise(request, category_id):
         for a in answers:
             if a == correctAnswers[counter]:
                 score += 20
-                all_answers.append([True,questionList[counter].question])
+                all_answers.append([True, questionList[counter].question])
             elif a != correctAnswers[counter]:
-                all_answers.append([False,questionList[counter].question])
-            counter+=1            
+                all_answers.append([False, questionList[counter].question])
+            counter += 1
         return render(request, 'main_app/score.html', {
-            'all_answers' : all_answers,
-            'score' : score 
+            'all_answers': all_answers,
+            'score': score
         })
 
 
@@ -167,28 +170,25 @@ def signup(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            current_site = get_current_site(request)  
-            mail_subject = 'Activation link has been sent to your email id'  
-            message = render_to_string('acc_active_email.html', {  
-                'user': user,  
-                'domain': current_site.domain,  
-                'uid':urlsafe_base64_encode(force_bytes(user.pk)),  
-                'token':account_activation_token.make_token(user),  
-            })  
-            to_email = form.cleaned_data.get('email')  
-            email = EmailMessage(  
-                        mail_subject, message, to=[to_email]  
-            )  
-            email.send()  
+            current_site = get_current_site(request)
+            mail_subject = 'Activation link has been sent to your email id'
+            message = render_to_string(
+                'acc_active_email.html', {
+                    'user': user,
+                    'domain': current_site.domain,
+                    'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                    'token': account_activation_token.make_token(user),
+                })
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(mail_subject, message, to=[to_email])
+            email.send()
             login(request, user)
             messages.success(
                 request,
                 f"Sign-up Successful! Welcome, {user.username}, to our community."
             )
             messages.success(
-                request,
-                f"Activation link has been sent to your email id."
-            )
+                request, f"Activation link has been sent to your email id.")
             return redirect("home")
         else:
             messages.error(request, "Invalid form entries")
@@ -259,51 +259,42 @@ class AddExercise(CreateView):
 class ContributeCategoryList(ListView):
     model = Category
     template_name = "main_app/contribute.html"
-############################
-
-# class NewPasswordResetView(PasswordResetView):
-#     form_class = PasswordResetForm
-#     success_url = '/login/'
-
-# class NewPasswordResetConfirmView(PasswordResetConfirmView):
-#     form_class = NewSetPasswordForm
-#     success_url = '/login/'
-    
 
 
 ################################################
 ##                 Email Verification         ##
 ################################################
 
-def activate(request, uidb64, token):  
-    user = CustomUser()  
-    try:  
-        uid = force_str(urlsafe_base64_decode(uidb64))  
-        user = CustomUser.objects.get(pk=uid)  
-    except(TypeError, ValueError, OverflowError, CustomUser.DoesNotExist):  
-        user = None  
-    if user is not None and account_activation_token.check_token(user, token):  
-        user.is_active = True  
+
+def activate(request, uidb64, token):
+    user = CustomUser()
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = CustomUser.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, CustomUser.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
         user.save()
-        messages.success(request, 'Your email has been confirmed. Welcome to our website!')
+        messages.success(
+            request, 'Your email has been confirmed. Welcome to our website!')
         return redirect('home')
-    else:  
-        return HttpResponse('Activation link is invalid!')   
-    
-    
-    
-    
-    
+    else:
+        return HttpResponse('Activation link is invalid!')
+
+################################################
+##                 Profile                    ##
+################################################
 class Profile(DetailView):
-    model = CustomUser  
+    model = CustomUser
     template_name = 'profile.html'
     slug_field = 'username'
     slug_url_kwarg = 'username'
 
     def get_context_data(self, **kwargs):
         context = super(Profile, self).get_context_data(**kwargs)
-        id = int(self.kwargs.get('pk')) 
+        id = int(self.kwargs.get('pk'))
         categories = UserCategory.objects.filter(user_id=id)
         print('categories', categories)
         context['userCategories'] = categories
-        return context 
+        return context
